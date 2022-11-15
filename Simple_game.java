e4bdf32e20db970d4b862f81220e1d16dee85481
vigneshks15 <107091826+vigneshks15@users.noreply.github.com>
2022-11-15 19:34:54 +0530

    Add files via upload

---
+++ Simple_game.java
@@ -0,0 +1,27 @@
+package main_project;
+
+public class Simple_game {
+	 public static void main(String[] args) {
+//	    Player1 player = new Player1("Nandha","Rifle",100);
+//        System.out.println(player.getName());
+//        System.out.println(player.getHealth());
+//        System.out.println(player.getWeapon());
+	    
+//	    player.damageByGun1();
+//	    player.damageByGun1();
+//	    player.damageByGun2();
+	    
+//	    player.damageByGun1();
+//	    player.damageByGun1();
+//	    player.damageByGun2();
+//		player.heal();
+	    
+//	    Player2 player2 = new Player2("Arun","Machine gun",80,true);
+//	    player2.damageByGun1();
+	    
+//	    Player2 player2 = new Player2("Arun","Machine gun",80,false);
+//	    player2.damageByGun1();
+	    
+	    }
+}
+
